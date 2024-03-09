ec24d16d9e754579fac436743d5eb34e8565140b
Ragulvijaypathi <161424429+Ragulvijaypathi@users.noreply.github.com>
2024-03-09 15:47:33 +0530

    Add files via upload

---
+++ wheather_API.py
@@ -0,0 +1,26 @@
+import requests
+
+def get_weather(api_key, city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        weather_description = data['weather'][0]['description']
+        temperature = data['main']['temp']
+        humidity = data['main']['humidity']
+        wind_speed = data['wind']['speed']
+        print(f"Weather in {city}:")
+        print(f"Description: {weather_description}")
+        print(f"Temperature: {temperature}°C")
+        print(f"Humidity: {humidity}%")
+        print(f"Wind Speed: {wind_speed} m/s")
+    else:
+        print("Failed to fetch weather data.")
+
+def main():
+    api_key = "140c68058988cd0787f9938d5020042d"
+    city = input("Enter city name: ")
+    get_weather(api_key, city)
+
+if __name__ == "__main__":
+    main()
